ed4151de49db9b32eee3b8524d5198c3df1b645c
Bryan Rangel <q572q333@wichita.edu>
2025-04-12 15:42:59 -0500

    Adds .cs folders for all tables.

--- Classical_Music_Library_Web_App/Models/MusicDbContext.cs
+++
@@ -1,9 +0,0 @@
-namespace Classical_Music_Library_Web_App.Models
-{
-    public class MusicDbContext
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
